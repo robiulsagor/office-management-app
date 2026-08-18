46d3f1a6375162771e8d4006dd40c295f6b59339
robiulsagor <robiul100.me@gmail.com>
2026-08-18 18:19:27 +0600

    feat: update AuthLayout padding for improved layout spacing

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-300 p-2">
+    <div className="min-h-screen flex items-center justify-center bg-gray-300 py-20 px-3">
       {children}
     </div>
   )
