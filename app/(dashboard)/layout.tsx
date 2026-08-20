70c7160003cf5b68a7c496a7c2e4953ff3af630c
robiulsagor <robiul100.me@gmail.com>
2026-08-20 22:03:34 +0600

    feat: dashboard layout only accessible after login

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,23 @@
-import Navbar from "@/components/layout/Navbar"
-import Sidebar from "@/components/layout/Sidebar"
-import React from "react"
 
-const DashboardLayout = ({children}: {children: React.ReactNode}) => {
+import Navbar from "@/components/layout/Navbar";
+import Sidebar from "@/components/layout/Sidebar";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <>
-      <Sidebar/>
-      <div>
-        <Navbar/>
-        {children}
-      </div>
-    </>
-  )
+      <main className="min-h-screen flex flex-row gap-3 bg-slate-200 p-3">
+        <Sidebar />
+        <div className="space-y-3 flex-1">
+          <Navbar />
+          <div className="w-full flex-1 bg-slate-50 shadow-xl rounded-xl p-5">
+            {children}
+          </div>
+        </div>
+      </main>
+  );
 }
-
-export default DashboardLayout
