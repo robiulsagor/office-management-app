0a0f1093811e78ee4c23e733b5df50e789a15c22
robiulsagor <robiul100.me@gmail.com>
2026-08-24 17:17:03 +0600

    refactor: improve layout structure and styling for better responsiveness

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,5 @@
-
 import Navbar from "@/components/layout/Navbar";
 import Sidebar from "@/components/layout/Sidebar";
-
-
 
 export default function RootLayout({
   children,
@@ -10,9 +7,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <main className="min-h-screen flex flex-row gap-3 bg-slate-200 p-3">
+      <main className="min-h-screen bg-slate-200 p-3 w-full md:flex md:gap-3 border ">
         <Sidebar />
-        <div className="space-y-3 flex-1">
+        <div className="space-y-3 flex-1 w-full">
           <Navbar />
           <div className="w-full flex-1 bg-slate-50 shadow-xl rounded-xl p-5">
             {children}
