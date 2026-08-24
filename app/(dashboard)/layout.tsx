729981fc0a6e079653b0ddb66b488ae5080d9a46
robiulsagor <robiul100.me@gmail.com>
2026-08-24 21:54:30 +0600

    fix: adjust layout for consistent height and enable overflow handling

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,11 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <main className="min-h-screen bg-slate-200 p-3 w-full md:flex md:gap-3 border ">
+      <main className="h-screen bg-slate-200 p-3 w-full md:flex md:gap-3 border ">
         <Sidebar />
         <div className="space-y-3 flex-1 w-full">
           <Navbar />
-          <div className="w-full flex-1 bg-slate-50 shadow-xl rounded-xl p-5">
+          <div className="w-full flex-1 bg-slate-50 shadow-xl rounded-xl p-5 max-h-[calc(100vh-120px)] overflow-y-auto">
             {children}
           </div>
         </div>
