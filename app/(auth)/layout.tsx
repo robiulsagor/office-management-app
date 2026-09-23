d10f0e1cd400786354a2b50a9e7df9c6849c9b36
robiulsagor <robiul100.me@gmail.com>
2026-09-23 21:17:01 +0600

    feat: add title element to AuthLayout for improved SEO and user experience

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return (
+    <>
+    <title>Please Login</title>
     <div className="min-h-screen flex items-center justify-center bg-gray-300 py-20 px-3">
       {children}
     </div>
+    </>
   )
 }
 
